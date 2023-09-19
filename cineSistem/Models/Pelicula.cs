1f7706d4dc59c2d49028de75aadeb8aadfa6eae5
pilaruwu <yeisonpl2017@gmail.com>
2023-09-18 22:52:00 -0500

    Tin

---
+++ cineSistem/Models/Pelicula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cineSistem.Models;
+
+public partial class Pelicula
+{
+    public int IdPelicula { get; set; }
+
+    public string? Nomb { get; set; }
+
+    public string? Desc { get; set; }
+
+    public string? Direc { get; set; }
+
+    public string? Genero { get; set; }
+
+    public string? Imagen { get; set; }
+
+    public virtual ICollection<RankingPelicula> RankingPeliculas { get; set; } = new List<RankingPelicula>();
+}
